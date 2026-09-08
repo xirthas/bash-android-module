9910ffb3fa4ba04826e13c60d878ae5956e875a7
XIRTHAS <xirthas@gmail.com>
2026-09-08 20:11:29 +0300

    revert

--- .github/run-tests-android.sh
+++ .github/run-tests-android.sh
@@ -1,15 +1,44 @@
-#!/bin/sh
+#!/system/bin/sh
 set -e
-cd /home/tests
+cd /data/local/tmp/tests
 
-ls -la /
+find . -type f \( -name "run-*" -o -name "*.tests" -o -name "*.sub" -o -name "*.right" -o -name "execscript" -o -name "history.list" \) | xargs sed -i -E \
+    -e "s@(^|[^a-zA-Z0-9._])/tmp([^a-zA-Z0-9._-]|$)@\1/data/local/tmp\2@g" \
+    -e "s@(^|[^a-zA-Z0-9._])/bin([^a-zA-Z0-9._-]|$)@\1/data/local/tmp/bin\2@g" \
+    -e "s@(^|[^a-zA-Z0-9._])/etc([^a-zA-Z0-9._-]|$)@\1/data/local/tmp/etc\2@g" \
+    -e "s@(^|[^a-zA-Z0-9._])/usr([^a-zA-Z0-9._-]|$)@\1/data/local/tmp/usr\2@g" \
+    -e "s@(^|[^a-zA-Z0-9._])/sbin([^a-zA-Z0-9._-]|$)@\1/data/local/tmp/sbin\2@g"
 
+find . -type f \( -name "run-*" -o -name "*.tests" -o -name "*.sub" -o -name "*.right" -o -name "execscript" -o -name "history.list" \) | xargs sed -i -E \
+    -e 's@(^|[^a-zA-Z0-9._])\\tmp/([a-zA-Z0-9._-]|$)@\1\\data/local/tmp/\2@g' \
+    -e 's@(^|[^a-zA-Z0-9._])\\bin/([a-zA-Z0-9._-]|$)@\1\\data/local/tmp/bin/\2@g' \
+    -e 's@(^|[^a-zA-Z0-9._])\\etc/([a-zA-Z0-9._-]|$)@\1\\data/local/tmp/etc/\2@g' \
+    -e 's@(^|[^a-zA-Z0-9._])\\usr/([a-zA-Z0-9._-]|$)@\1\\data/local/tmp/usr/\2@g' \
+    -e 's@(^|[^a-zA-Z0-9._])\\sbin/([a-zA-Z0-9._-]|$)@\1\\data/local/tmp/sbin/\2@g'
+
+find . -type f \( -name "run-*" -o -name "*.tests" -o -name "*.sub" -o -name "*.right" -o -name "execscript" -o -name "history.list" \) | xargs sed -i -E \
+    -e 's@(^|[^a-zA-Z0-9._])\\tmp([^a-zA-Z0-9._-]|$)@\1\\data\\local\\tmp\2@g' \
+    -e 's@(^|[^a-zA-Z0-9._])\\bin([^a-zA-Z0-9._-]|$)@\1\\data\\local\\tmp\\bin\2@g' \
+    -e 's@(^|[^a-zA-Z0-9._])\\etc([^a-zA-Z0-9._-]|$)@\1\\data\\local\\tmp\\etc\2@g' \
+    -e 's@(^|[^a-zA-Z0-9._])\\usr([^a-zA-Z0-9._-]|$)@\1\\data\\local\\tmp\\usr\2@g' \
+    -e 's@(^|[^a-zA-Z0-9._])\\sbin([^a-zA-Z0-9._-]|$)@\1\\data\\local\\tmp\\sbin\2@g'
+
+find . -type f \( -name "run-*" -o -name "*.tests" -o -name "*.sub" -o -name "*.right" -o -name "execscript" -o -name "history.list" \) | xargs sed -i -E \
+    -e "s@(^|[^a-zA-Z0-9._])\^tmp([^a-zA-Z0-9._-]|$)@\1\^data\^local\^tmp\2@g" \
+    -e "s@(^|[^a-zA-Z0-9._])\^bin([^a-zA-Z0-9._-]|$)@\1\^data\^local\^tmp\^bin\2@g" \
+    -e "s@(^|[^a-zA-Z0-9._])\^etc([^a-zA-Z0-9._-]|$)@\1\^data\^local\^tmp\^etc\2@g" \
+    -e "s@(^|[^a-zA-Z0-9._])\^usr([^a-zA-Z0-9._-]|$)@\1\^data\^local\^tmp\^usr\2@g" \
+    -e "s@(^|[^a-zA-Z0-9._])\^sbin([^a-zA-Z0-9._-]|$)@\1\^data\^local\^tmp\^sbin\2@g"
+
+sed -i "s|/data/local/tmp/usr/bin/printf|/data/local/tmp/bin/printf|g" run-* *.tests *.sub *.right execscript history.list misc/*.tests
+
+sed -i "s|ln sh a|cp sh a|g" rsh2.sub
 sed -i 's|\[\[ x =~ \${bs}x \]\] ; echo \$?|[[ x =~ x ]] ; echo $?|g' cond-regexp3.sub
 
-export PATH=/bin:$PATH
-export TMPDIR=/tmp
-export THIS_SH=/bin/bash
-export USER=shell
-export HOME=/home
+echo "root:x:0:0:root:/root:/bin/sh" > /data/local/tmp/etc/passwd
 
-/usr/bin/su -p -s /bin/bash shell -c "export PATH=/bin:\$PATH; export TMPDIR=/tmp; export THIS_SH=/bin/bash; /bin/bash run-all < /dev/null"
+export PATH=/data/local/tmp/bin:$PATH
+export TMPDIR=/data/local/tmp
+export THIS_SH=/data/local/tmp/bin/bash
+
+${THIS_SH} run-all < /dev/null
