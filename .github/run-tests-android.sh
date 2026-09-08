49bd05c461ebeb62e3f2c3d7b67be83a4533a24a
XIRTHAS <xirthas@gmail.com>
2026-09-08 22:44:01 +0300

    history.list

--- .github/run-tests-android.sh
+++ .github/run-tests-android.sh
@@ -2,7 +2,7 @@
 set -e
 cd /data/local/tmp/tests
 
-find . -type f \( -name "run-*" -o -name "*.tests" -o -name "*.sub" -o -name "*.right" -o -name "execscript" -o -name "history.list" \) \
+find . -type f \( -name "run-*" -o -name "*.tests" -o -name "*.sub" -o -name "*.right" -o -name "execscript" \) \
     -not -name 'array*.*' -not -name 'assoc*.*' -not -name 'new-exp*.*' -not -name 'histexp*.*' | xargs sed -i -E \
     -e ":a" \
     -e "s@(^|[^a-zA-Z0-9._])/tmp([^a-zA-Z0-9._-]|$)@\1/data/local/tmp\2@g" \
@@ -12,7 +12,7 @@ find . -type f \( -name "run-*" -o -name "*.tests" -o -name "*.sub" -o -name "*.
     -e "s@(^|[^a-zA-Z0-9._])/sbin([^a-zA-Z0-9._-]|$)@\1/data/local/tmp/sbin\2@g" \
     -e "ta"
 
-sed -i "s|/data/local/tmp/usr/bin/printf|/data/local/tmp/bin/printf|g" run-* *.tests *.sub *.right execscript history.list misc/*.tests
+sed -i "s|/data/local/tmp/usr/bin/printf|/data/local/tmp/bin/printf|g" run-* *.tests *.sub *.right execscript misc/*.tests
 
 sed -i "s|ln sh a|cp sh a|g" rsh2.sub
 sed -i 's|\[\[ x =~ \${bs}x \]\] ; echo \$?|[[ x =~ x ]] ; echo $?|g' cond-regexp3.sub
