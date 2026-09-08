e11ac1d230640074b3ba34521dcec733bd0ad876
XIRTHAS <xirthas@gmail.com>
2026-09-08 21:56:28 +0300

    repeat

--- .github/run-tests-android.sh
+++ .github/run-tests-android.sh
@@ -4,11 +4,13 @@ cd /data/local/tmp/tests
 
 find . -type f \( -name "run-*" -o -name "*.tests" -o -name "*.sub" -o -name "*.right" -o -name "execscript" -o -name "history.list" \) \
     -not -name 'array*.*' -not -name 'assoc*.*' | xargs sed -i -E \
+    -e ":a" \
     -e "s@(^|[^a-zA-Z0-9._])/tmp([^a-zA-Z0-9._-]|$)@\1/data/local/tmp\2@g" \
     -e "s@(^|[^a-zA-Z0-9._])/bin([^a-zA-Z0-9._-]|$)@\1/data/local/tmp/bin\2@g" \
     -e "s@(^|[^a-zA-Z0-9._])/etc([^a-zA-Z0-9._-]|$)@\1/data/local/tmp/etc\2@g" \
     -e "s@(^|[^a-zA-Z0-9._])/usr([^a-zA-Z0-9._-]|$)@\1/data/local/tmp/usr\2@g" \
-    -e "s@(^|[^a-zA-Z0-9._])/sbin([^a-zA-Z0-9._-]|$)@\1/data/local/tmp/sbin\2@g"
+    -e "s@(^|[^a-zA-Z0-9._])/sbin([^a-zA-Z0-9._-]|$)@\1/data/local/tmp/sbin\2@g" \
+    -e "ta"
 
 sed -i "s|/data/local/tmp/usr/bin/printf|/data/local/tmp/bin/printf|g" run-* *.tests *.sub *.right execscript history.list misc/*.tests
 
