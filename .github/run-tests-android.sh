1c6d70e2da1818fc60b4e3b85d2e6b673fc9daa7
XIRTHAS <xirthas@gmail.com>
2026-09-08 23:03:53 +0300

    histexp

--- .github/run-tests-android.sh
+++ .github/run-tests-android.sh
@@ -18,6 +18,7 @@ sed -i "s|ln sh a|cp sh a|g" rsh2.sub
 sed -i 's|\[\[ x =~ \${bs}x \]\] ; echo \$?|[[ x =~ x ]] ; echo $?|g' cond-regexp3.sub
 sed -i "s|!?d?:5|!?b c d?:5|g" histexp.tests
 sed -i "s|/bin/sh|/data/local/tmp/bin/sh|g" histexp.right
+sed -i "s|/bin$|/data/local/tmp/bin|g" histexp.right
 
 echo "echo SKIPPED" > run-intl
 
