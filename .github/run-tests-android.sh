c4d470afafd95e9586aae2f8f44e2b1dba391621
XIRTHAS <xirthas@gmail.com>
2026-09-08 19:40:05 +0300

    test

--- .github/run-tests-android.sh
+++ .github/run-tests-android.sh
@@ -12,4 +12,4 @@ export THIS_SH=/bin/bash
 export USER=shell
 export HOME=/tmp
 
-/usr/bin/su -p shell /bin/bash -c "export PATH=/bin:\$PATH; export TMPDIR=/tmp; export THIS_SH=/bin/bash; /bin/bash run-all < /dev/null"
+/usr/bin/su -p -s /bin/bash shell -c "export PATH=/bin:\$PATH; export TMPDIR=/tmp; export THIS_SH=/bin/bash; /bin/bash run-all < /dev/null"
