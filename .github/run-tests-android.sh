5e2f93c628c573771df154ea0e50f38907c4fb58
XIRTHAS <xirthas@gmail.com>
2026-09-09 00:33:40 +0300

    env TMPDIR=$TMPDIR

--- .github/run-tests-android.sh
+++ .github/run-tests-android.sh
@@ -18,7 +18,7 @@ sed -i 's|\[\[ x =~ \${bs}x \]\] ; echo \$?|[[ x =~ x ]] ; echo $?|g' cond-regex
 sed -i 's|!?d?:5|!?b c d?:5|g' histexp.tests
 sed -i "s|/bin/sh|/data/local/tmp/bin/sh|g" histexp.right
 sed -i "s|/bin$|/data/local/tmp/bin|g" histexp.right
-sed -i 's|diff <("$t1") <("$t2")|TMPDIR=$TMPDIR diff <("$t1") <("$t2")|g' shopt1.sub
+sed -i 's|diff <("$t1") <("$t2")|env TMPDIR=$TMPDIR diff <("$t1") <("$t2")|g' shopt1.sub
 
 echo "echo SKIPPED" > run-intl
 
