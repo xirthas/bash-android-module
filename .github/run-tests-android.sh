364f99e39f8fdbf656bf73d966d711989ea5e0ed
XIRTHAS <xirthas@gmail.com>
2026-09-08 22:30:18 +0300

    test

--- .github/run-tests-android.sh
+++ .github/run-tests-android.sh
@@ -3,7 +3,7 @@ set -e
 cd /data/local/tmp/tests
 
 find . -type f \( -name "run-*" -o -name "*.tests" -o -name "*.sub" -o -name "*.right" -o -name "execscript" -o -name "history.list" \) \
-    -not -name 'array*.*' -not -name 'assoc*.*' | xargs sed -i -E \
+    -not -name 'array*.*' -not -name 'assoc*.*' -not -name 'new-exp*.*' | xargs sed -i -E \
     -e ":a" \
     -e "s@(^|[^a-zA-Z0-9._])/tmp([^a-zA-Z0-9._-]|$)@\1/data/local/tmp\2@g" \
     -e "s@(^|[^a-zA-Z0-9._])/bin([^a-zA-Z0-9._-]|$)@\1/data/local/tmp/bin\2@g" \
@@ -18,7 +18,7 @@ sed -i "s|ln sh a|cp sh a|g" rsh2.sub
 sed -i 's|\[\[ x =~ \${bs}x \]\] ; echo \$?|[[ x =~ x ]] ; echo $?|g' cond-regexp3.sub
 sed -i "s|!?d?:5|!?b c d?:5|g" histexp.tests
 
-echo "" > run-intl
+echo "echo SKIPPED" > run-intl
 
 echo "root:x:0:0:root:/root:/bin/sh" > /data/local/tmp/etc/passwd
 
